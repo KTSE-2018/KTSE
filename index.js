de7cc17a7ba4a3fe88c7774a115a9cefbf0d60e8
julesnuggy <julesnuggy@gmail.com>
2018-05-01 17:32:55 +0100

    MVP - can move square up, down, left, right

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(8000, function () {
