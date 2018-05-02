3418491e9f71b102891bd78d40d7ededfca305fe
Alfie Darko <me@alfiedarko.co.uk>
2018-05-02 18:29:20 +0100

    Passing test: ActionPoints > #points > should start with 10 points

--- test/actionPointsTest.js
+++ test/actionPointsTest.js
@@ -1,11 +1,10 @@
 describe('ActionPoints', function() {
-  beforeEach(function() {
-    ap = new ActionPoints();
-  })
 
   describe('#points', function() {
     it('should start with 10 points ', function() {
-      expect(ap.points).to.eq(10);
+      ap = new ActionPoints();
+
+      chai.expect(ap.points).to.eq(10);
     });
   });
 });
