5958a5b46c07f1c094670817c440793e354edd77
Thomas Duffy <tomjamesduffy@gmail.com>
2018-05-03 17:16:22 +0100

    Player is now flush with objects

--- public/src/collisionOnD.js
+++ public/src/collisionOnD.js
@@ -1,16 +1,19 @@
 (function(exports) {
     collisionOnD = function(a, b, c) {
       var x
+      var obj
       b.some(function(e) {
           if (
-            ((a._y > (e._y + e._spriteHeight)) ||
-            ((a._y + a._spriteHeight + c) < e._y) ||
-            ((a._x + a._spriteWidth) < e._x) ||
-            (a._x > (e._x + e._spriteWidth))) === false) {
+            ((a._y >= (e._y + e._spriteHeight)) ||
+            ((a._y + a._spriteHeight + c) <= e._y) ||
+            ((a._x + a._spriteWidth) <= e._x) ||
+            (a._x >= (e._x + e._spriteWidth))) === false) {
           x = true
+          obj = e
+          return x, obj
         }
       })
-    return x
+    return { 'collide': x, 'object': obj }
   }
   exports.collisionOnD = collisionOnD;
 })(this)
