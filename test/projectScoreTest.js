0cd12005b1d5075de2d5fd3662c935162bea3c26
Alfie Darko <me@alfiedarko.co.uk>
2018-05-03 13:36:25 +0100

    Passing Test: ProjectScore > #score > initiates with an integer of 0

--- test/projectScoreTest.js
+++ test/projectScoreTest.js
@@ -2,7 +2,7 @@ describe('ProjectScore', function() {
   describe('#score', function() {
     it('initiates with an integer of 0', function() {
       ps = new ProjectScore
-      expect(ps.score).to.eq(0);
+      chai.expect(ps.score).to.eq(0);
     });
   });
 });
