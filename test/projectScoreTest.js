b73cbee44ebd6f8341231929f7436d406c79f608
Alfie Darko <me@alfiedarko.co.uk>
2018-05-03 13:38:56 +0100

    Failing Test: ProjectScore > .increaseProjectScore > increases ProjectScore #score by 1

--- test/projectScoreTest.js
+++ test/projectScoreTest.js
@@ -5,4 +5,13 @@ describe('ProjectScore', function() {
       chai.expect(ps.score).to.eq(0);
     });
   });
+
+  describe('.increaseProjectScore', function() {
+    it('increases ProjectScore #score by 1', function() {
+
+      ps = new ProjectScore
+      ps.increaseProjectScore()
+      chai.expect(ps.score).to.eq(1);
+    });
+  });
 });
