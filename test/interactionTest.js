83653f621ea0d5b91006b7f34aaca04d3bed93dd
julesnuggy <julesnuggy@gmail.com>
2018-05-04 14:26:31 +0100

    GREEN: first basic test for interaction passed

--- test/interactionTest.js
+++ test/interactionTest.js
@@ -1,18 +1,28 @@
-describe('collisionLogic', function() {
+describe('Player Interaction', function() {
   var player, collide_object, message_box
 
   beforeEach(function() {
     player = new Player();
-    collide_object = {id: 'npc'};
-    collision_return = {'collide': true, 'object': collide_object}
+    collisionLogic = new CollisionLogic
+    consoleSpy = sinon.spy(console, 'log');
+    collisionStub = sinon.stub(collisionLogic, "collision")
+    collision_box = {
+      'id': 'npc'
+    };
+    collision_return = {
+      'collide': true,
+      'object': collision_box
+    }
+    collisionStub.returns(collision_return)
   })
 
-  it('should cause player to interact with object on collision', function() {
+  it('should allow a player to interact with an object on collision', function() {
     // Arrange
-
+    player._rightPressed = true;
     // Action
-
+    player.reposition(player);
     // Assert
+    consoleSpy.returned("MESSAGE")
 
   })
 })
