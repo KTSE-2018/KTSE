0aea24b3e703986821cab22e13edb6b33fec28c3
Thomas Duffy <tomjamesduffy@gmail.com>
2018-05-06 12:28:37 +0100

    Failing test, dialogue step returns dialogue that matches the count

--- test/dialogueBoxTest.js
+++ test/dialogueBoxTest.js
@@ -5,11 +5,27 @@ describe('DialogueBox', function() {
     var expect = chai.expect
     var dialogueBox = new DialogueBox()
 
-    it('dialogue box should only persist whilst it is in use', function() {
+    it('persist while _inuse is true', function() {
       var drawSpy = sinon.spy(dialogueBox, "drawDialogueBox");
       dialogueBox._inUse = true;
       dialogueBox.reposition()
       expect(drawSpy.callCount).to.eq(1)
+      drawSpy.restore();
+    });
+
+    it('does not persist whilst _inuse is false', function() {
+      var drawSpy = sinon.spy(dialogueBox, "drawDialogueBox");
+      dialogueBox._inUse = false;
+      dialogueBox.reposition()
+      expect(drawSpy.callCount).to.eq(0)
+      drawSpy.restore();
     });
   })
+
+  describe('#dialogueStep', function() {
+    it('returns the dialogue that matches the count', function() {
+      dialogueBox._count += 1
+      expect(dialogueBox.dialogueStep()).to.eq("MORE engaging dialogue")
+      })
+    })
 })
