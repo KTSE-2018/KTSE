04de809fe331378231187ab41789efeca67408a2
Thomas Duffy <tomjamesduffy@gmail.com>
2018-05-06 11:58:27 +0100

    First failing test, dialogue box not defined

---
+++ test/dialogueBoxTest.js
@@ -0,0 +1,12 @@
+describe('DialogueBox', function() {
+
+  describe('#create', function() {
+
+    var expect = chai.expect
+    var dialogueBox = new DialogueBox()
+
+    it('dialogue box class exists', function() {
+      expect(dialogueBox).to.eq(dialogueBox)
+    });
+  })
+})
