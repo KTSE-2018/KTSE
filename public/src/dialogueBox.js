b6e17efea40e42e48de5dc45b5c3ddfadf85a67b
Thomas Duffy <tomjamesduffy@gmail.com>
2018-05-06 12:05:37 +0100

    Added method to draw a custom message box to the screen

--- public/src/dialogueBox.js
+++ public/src/dialogueBox.js
@@ -1,11 +1,35 @@
 function DialogueBox() {
-    this._canvas = document.getElementById("myCanvas");
-    this._ctx = this._canvas.getContext("2d");
-    this._x = 50;
-    this._y = this._canvas.height - 150;
-    this._rectHeight = 100;
-    this._rectWidth = this._canvas.width*0.8;
-    this._inUse = false;
-    this._dialogue = [{'m':'Engaging dialogue'}, {'m':'MORE engaging dialogue'}];
-    this._count = 0;
+  this._canvas = document.getElementById("myCanvas");
+  this._ctx = this._canvas.getContext("2d");
+  this._x = 50;
+  this._y = this._canvas.height - 150;
+  this._rectHeight = 100;
+  this._rectWidth = this._canvas.width * 0.8;
+  this._inUse = false;
+  this._dialogue = [{
+    'm': 'Engaging dialogue'
+  }, {
+    'm': 'MORE engaging dialogue'
+  }];
+  this._count = 0;
+
+  DialogueBox.prototype.drawDialogueBox = function() {
+    this._ctx.beginPath();
+    this.drawTextBG(this._ctx, this.dialogueStep(), '20px arial', this._x, this._y);
+    this._ctx.closePath();
+  }
+
+  DialogueBox.prototype.drawTextBG = function(ctx, txt, font, x, y) {
+    ctx.save();
+    ctx.font = font;
+    ctx.textBaseline = 'top';
+    ctx.fillStyle = 'white';
+    var width = ctx.measureText(txt).width + 10;
+    ctx.fillRect(x, y, width, parseInt(font, 10));
+    ctx.fillStyle = '#000';
+    ctx.fillText(txt, x, y);
+    ctx.restore();
+  }
+
+
 }
