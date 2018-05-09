1c9f997ff5bd36572a1775d7097c1eb8fb706f26
Thomas Duffy <tomjamesduffy@gmail.com>
2018-05-09 13:04:24 +0100

    Added new jobs, fixed logic

--- public/src/energyLevel.js
+++ public/src/energyLevel.js
@@ -1,4 +1,5 @@
 function EnergyLevel(game) {
+  this.game = game
   this._points = 100;
 }
 
@@ -8,7 +9,7 @@ EnergyLevel.prototype.increaseEnergy = function(x = 30) {
 
 EnergyLevel.prototype.decreaseEnergy = function(x = 35) {
   this._points -= x;
-  if (this._points <= 0) {
+  if (this._points <= 0 && this.game.actionPoints._points <= 0) {
     game.gameOver("e");
   }
 };
