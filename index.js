22a05550e59385336ee1d3f170746717be100c5b
Christopher Ly <demedial@gmail.com>
2018-05-09 10:11:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
+
+app.set("port", process.env.PORT || 8000);
 
 var options = {
-  root: __dirname + '/public'
-}
+  root: __dirname + "/public"
+};
 
-app.get('/game', function (req, res) {
-  res.sendFile('/views/game.html', options)
-})
+app.get("/game", function(req, res) {
+  res.sendFile("/views/game.html", options);
+});
 
-app.get('/', function (req, res) {
-  res.sendFile('/views/index.html', options)
-})
+app.get("/", function(req, res) {
+  res.sendFile("/views/index.html", options);
+});
 
-app.get('/rules', function (req, res) {
-  res.sendFile('/views/rules.html', options)
-})
+app.get("/rules", function(req, res) {
+  res.sendFile("/views/rules.html", options);
+});
 
-app.listen(process.env.port || 8000, function () {
-  console.log('Example app listening on port 8000!')
-})
+app.listen(app.get("port"), function() {
+  console.log("Example app listening on port", app.get("port"));
+});
